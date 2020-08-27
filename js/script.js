5ade858d7e30c3072b6ddaba69ddcfc9a6b5bff9
Ira <irarad1994@gmail.com>
2020-08-28 00:02:55 +0300

    edited

--- js/script.js
+++ js/script.js
@@ -1,63 +1,42 @@
 const getCharactersButtons = document.getElementById("btn--get_charachters");
 const aboutСharachters = document.getElementById("about-charachters");
-// const prev = document.getElementById('prev');
+const btnPrev = document.querySelector("#prev");
+const btnNext = document.querySelector("#next");
+const getPlanets = document.getElementById("get_planets");
 const BASE = "https://swapi.dev/api/";
 const getFilmUrl = (num) => `${BASE}films/${num}/`;
 const planet = `${BASE}planets/`;
 const toHTTPS = (url) =>
   url[4].toLowerCase() === "s" ? url : `${url.slice(0, 4)}s${url.slice(4)}`;
-
 function sendRequest(url) {
   const newURL = toHTTPS(url);
   return fetch(newURL)
     .then((res) => res.json())
     .catch("console.log");
 }
-
 let film;
-
 const selectEpisode = document.getElementById("select-episode");
-getCharactersButtons.addEventListener("click", () => {
-  var episode = selectEpisode.value;
-  switch (episode) {
-    case "episode1":
-      film = getFilmUrl(1);
-      break;
-    case "episode2":
-      film = getFilmUrl(2);
-      break;
-    case "episode3":
-      film = getFilmUrl(3);
-      break;
-    case "episode4":
-      film = getFilmUrl(4);
-      break;
-    case "episode5":
-      film = getFilmUrl(5);
-      break;
-    case "episode6":
-      film = getFilmUrl(6);
-      break;
-  }
-
-  sendRequest(film).then((data) => {
+let currentValue = 1;
+selectEpisode.addEventListener("click", (e) => {
+  currentValue = Number(e.target.value.slice(-1));
+});
+getCharactersButtons.addEventListener("click", (e) => {
+  sendRequest(getFilmUrl(currentValue)).then((data) => {
     let character = "";
     let i = 0;
     while (i < data.characters.length) {
       i++;
       sendRequest(data.characters[i]).then((aboutCharter) => {
         character += `<div class="character-info">
-                                            <h3> Сharacter name - ${aboutCharter.name}. 
-                                            Birthday- ${aboutCharter.birth_year}.
-                                            Gender - ${aboutCharter.gender}. </h3>
-                            
+                                            <h3> Сharacter name - ${aboutCharter.name}. </h3> 
+                                            <h3> Birthday- ${aboutCharter.birth_year}. </h3> 
+                                            <h3> Gender - ${aboutCharter.gender}. </h3>                            
                                   </div>`;
         aboutСharachters.innerHTML = `${character}`;
       });
     }
   });
 });
-
 let page = 1;
 function getPlanet(page) {
   const config = {
@@ -68,45 +47,44 @@ function getPlanet(page) {
     },
   };
   return axios(config).then((res) => res.data.results);
-//   return res.data.results;
+  //   return res.data.results;
 }
-
-function renderPlanets(planets) {
+function showElem(elem) {
+  elem.style.display = "block";
+}
+function hideElem(elem) {
+  elem.style.display = "none";
+}
+getPlanets.addEventListener("click", () => {
+  showElem(btnNext);
+  showElem(btnPrev);
+  getPlanet(page).then((res) => showPlanets(res));
+});
+function showPlanets(data) {
   const containerPlanets = document.querySelector(".container__third--planets");
   containerPlanets.innerHTML = "";
-
-  document.getElementById("get_planets").addEventListener("click", () => {
-    document.querySelector("#prev").style.display = "block";
-    document.querySelector("#next").style.display = "block";
-
-    planets.forEach((planet) => {
-      const planetElement = document.createElement("div");
-      planetElement.innerHTML = `
-        <h3 class="planets__name"> Name of the planet - ${planet.name}</h3>
-        <h3 class="planets__name"> Population - ${planet.population}</h3>
-        <h3 class="planets__name"> Climate - ${planet.climate}</h3> 
-        `;
-      containerPlanets.append(planetElement);
-    });
+  data.map((planet) => {
+    const planetElement = document.createElement("div");
+    planetElement.classList.add("planet");
+    planetElement.innerHTML = `
+      <h3 class="planets__name"> Name of the planet - ${planet.name}</h3>
+      <h3 class="planets__name"> Population - ${planet.population}</h3>
+      <h3 class="planets__name"> Climate - ${planet.climate}</h3>
+      `;
+    containerPlanets.appendChild(planetElement);
   });
-
-  
 }
-getPlanet().then(renderPlanets);
- 
-function changePlanets() {
-   
-    document.getElementById('prev').addEventListener('click', () => {
-      if (page <= 1) {
-          return;
-      }else
-      getPlanet(--page).then(renderPlanets);
-    });
-    document.getElementById('next').addEventListener('click', () => {
-        if(page >= 6){
-            return
-        }else
-        getPlanet(++page).then(renderPlanets);
-    });
+btnNext.addEventListener("click", () => {
+  if (page > 5) {
+    return;
   }
-  changePlanets()
+  page++;
+  getPlanet(page).then((res) => showPlanets(res));
+});
+btnPrev.addEventListener("click", () => {
+  if (page < 1) {
+    return;
+  }
+  page--;
+  getPlanet(page).then((res) => showPlanets(res));
+});
